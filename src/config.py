e59357ca1c06e1447b9e988258ae1c7689c6a20a
SHOTARO HAYAKAWA <Shotaro.Hayakawa@ibm.com>
2021-04-19 20:40:53 +0900

    modified path

--- src/config.py
+++ src/config.py
@@ -1,10 +1,12 @@
 import os
 from pathlib import Path
 
-repo_dir = str(Path(os.getcwd()).parent)
+repo_dir = str(Path(__file__).parent.resolve())
 
 MODEL_DIR = os.path.join(repo_dir, 'models')
 MODEL_VERSION = 0.1
 MODEL_VERSION_NOTE = "supervised learing model for time-series"
 
 LOG_DIR = os.path.join(repo_dir, 'logs')
+
+DATA_DIR = os.path.join(repo_dir, 'data')
